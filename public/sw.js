5aa4d503418f331320d37fd65af42fb51dec691c
vedavikas2216-bit <vedavikas2216@gmail.com>
2026-07-24 11:36:12 +0530

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => new Response('You are offline.'))
+  );
+});
